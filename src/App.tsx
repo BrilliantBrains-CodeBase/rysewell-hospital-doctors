a4b1d51cf72c1bc071a09b2ef240fc976a9742fc
Prashanth Naroju <prashanth.naro@gmail.com>
2026-01-03 19:20:53 +0530

    inital repo setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/home/Home"
+import NotFound from "./pages/NotFound"
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <div className="text-red-500 text-4xl">
+       this is home page
+    </div>
+  )
+}
+
+export default Home
